f94536fe688441a12a84f0b72e98651ae96a9f85
Rafal <rmarkowski97@gmail.com>
2024-05-05 16:36:53 +0200

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        String value = String.valueOf(x);
-        StringBuilder builder = new StringBuilder(value);
-        return builder.reverse().toString().equals(value);
-    }
-}
